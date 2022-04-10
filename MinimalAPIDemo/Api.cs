131320464c168f502a2fed4bdf3c643a36533e9d
1973-clinton <clintonboamahofficial@gmail.com>
2022-04-10 21:38:47 +0000

    Configure Api endpoints

--- MinimalAPIDemo/Api.cs
+++ MinimalAPIDemo/Api.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccess.Data;
+using DataAccess.Models;
 
 namespace MinimalAPIDemo
 {
@@ -6,7 +7,11 @@ namespace MinimalAPIDemo
     {
         public static void ConfigureApi(this WebApplication app)
         {
-
+            app.MapGet("/Users", GetUsers);
+            app.MapGet("/Users/{id}", GetUser);
+            app.MapDelete("/Users", DeleteUser);
+            app.MapPut("/Users", UpdateUser);
+            app.MapPost("/Users", InsertUser);
         }
 
         private static async Task<IResult> GetUsers(IUserData data)
@@ -20,5 +25,55 @@ namespace MinimalAPIDemo
                 return Results.Problem(ex.Message);
             }
         }
+
+        private static async Task<IResult> GetUser(int id, IUserData data)
+        {
+            var results = await data.GetUser(id);
+            if (results == null) return Results.NotFound();
+
+            return Results.Ok(results);
+
+        }
+
+        private static async Task<IResult> InsertUser(UserModel user, IUserData data)
+        {
+            try
+            {
+                await data.InsertUser(user);
+                return Results.Ok();
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+
+        private static async Task<IResult> UpdateUser(UserModel user, IUserData data)
+        {
+            try
+            {
+                await data.UpdateUser(user);
+                return Results.Ok();
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+
+        private static async Task<IResult> DeleteUser(int id, IUserData data)
+        {
+            try
+            {
+                await data.DeleteUser(id);
+                return Results.Ok();
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+
+
     }
 }
