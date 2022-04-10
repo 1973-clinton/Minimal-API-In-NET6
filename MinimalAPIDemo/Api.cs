422479957c7e8703bb6351facdaeb87e77fc418b
1973-clinton <clintonboamahofficial@gmail.com>
2022-04-10 20:35:15 +0000

    Modified package references

---
+++ MinimalAPIDemo/Api.cs
@@ -0,0 +1,24 @@
+﻿using DataAccess.Data;
+
+namespace MinimalAPIDemo
+{
+    public static class Api
+    {
+        public static void ConfigureApi(this WebApplication app)
+        {
+
+        }
+
+        private static async Task<IResult> GetUsers(IUserData data)
+        {
+            try
+            {
+                return Results.Ok(await data.GetUsers());
+            }
+            catch (Exception ex) 
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+    }
+}
